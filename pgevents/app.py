9fa945a534068326f80835aeb294edd46931c4d3
peajayni <peajayni>
2020-06-12 23:29:34 +0100

    Add register, unregister and run functionality

--- pgevents/app.py
+++ pgevents/app.py
@@ -1,7 +1,62 @@
+from collections import defaultdict
+
 import psycopg2
-from psycopg2.extensions import connection
 
 
 class App:
-    def __init__(self, dsn: str):
-        self._connection: connection = psycopg2.connect(dsn)
+    def __init__(self, dsn):
+        self.connection = psycopg2.connect(dsn)
+        self.connection.set_isolation_level(
+            psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT
+        )
+        self.registry: dict = defaultdict(set)
+
+    def register(self, channel):
+        def decorator(func):
+            self._listen(channel)
+            self._register(channel, func)
+            return func
+
+        return decorator
+
+    def unregister(self, channel, func):
+        if self._unregister(channel, func):
+            self._unlisten(channel)
+
+    def _listen(self, channel):
+        if len(self.registry[channel]) != 0:
+            return
+
+        cursor = self.connection.cursor()
+        cursor.execute(f"LISTEN {channel}")
+
+    def _unlisten(self, channel):
+        if len(self.registry[channel]) != 0:
+            return
+
+        cursor = self.connection.cursor()
+        cursor.execute(f"UNLISTEN {channel}")
+
+    def _register(self, channel, func):
+        self.registry[channel].add(func)
+
+    def _unregister(self, channel, func):
+        try:
+            self.registry[channel].remove(func)
+            return True
+        except KeyError:
+            return False
+
+    def run(self):
+        while True:
+            self._tick()
+
+    def _tick(self):
+        self.connection.poll()
+        while self.connection.notifies:
+            notification = self.connection.notifies.pop(0)
+            self._dispatch(notification)
+
+    def _dispatch(self, notification):
+        for handler in self.registry[notification.channel]:
+            handler(notification.payload)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from unittest.mock import Mock, sentinel
+from unittest.mock import Mock, sentinel, call
 
 import psycopg2
 import pytest
@@ -15,6 +15,121 @@ def psycopg2_connect(monkeypatch):
 
 def test_app_makes_connection(psycopg2_connect):
     dsn = sentinel
-    App(dsn)
+    app = App(dsn)
 
     psycopg2_connect.assert_called_once_with(dsn)
+    app.connection.set_isolation_level.assert_called_once_with(
+        psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT
+    )
+
+
+def test_app_register(psycopg2_connect):
+    dsn = sentinel
+    app = App(dsn)
+    channel = "foo"
+
+    @app.register(channel)
+    def bar():
+        pass
+
+    expected = dict(foo=set([bar]))
+
+    assert app.registry == expected
+    app.connection.cursor().execute.assert_called_once_with(f"LISTEN {channel}")
+
+
+def test_app_duplicated_register(psycopg2_connect):
+    dsn = sentinel
+    app = App(dsn)
+    channel = "foo"
+
+    def bar():
+        pass
+
+    app.register(channel)(bar)
+    app.register(channel)(bar)
+
+    expected = dict(foo=set([bar]))
+
+    assert app.registry == expected
+    app.connection.cursor().execute.assert_called_once_with(f"LISTEN {channel}")
+
+
+def test_app_multiple_register(psycopg2_connect):
+    dsn = sentinel
+    app = App(dsn)
+    channel = "foo"
+
+    @app.register(channel)
+    def bar0():
+        pass
+
+    @app.register(channel)
+    def bar1():
+        pass
+
+    expected = dict(foo=set([bar0, bar1]))
+
+    assert app.registry == expected
+    app.connection.cursor().execute.assert_called_once_with(f"LISTEN {channel}")
+
+
+def test_app_unregister_when_registered(psycopg2_connect):
+    dsn = sentinel
+    app = App(dsn)
+    channel = "foo"
+
+    def bar():
+        pass
+
+    app.registry[channel].add(bar)
+
+    app.unregister(channel, bar)
+
+    assert app.registry[channel] == set()
+    app.connection.cursor().execute.assert_called_once_with(f"UNLISTEN {channel}")
+
+
+def test_app_unregister_when_not_registered(psycopg2_connect):
+    dsn = sentinel
+    app = App(dsn)
+    channel = "foo"
+
+    def bar():
+        pass
+
+    app.unregister(channel, bar)
+
+    assert app.registry[channel] == set()
+    app.connection.cursor().execute.assert_not_called()
+
+
+def test_run(psycopg2_connect):
+    dsn = sentinel
+    app = App(dsn)
+    channel = "foo"
+
+    handler = Mock()
+    notification0 = Mock()
+    notification0.channel = channel
+
+    notification1 = Mock()
+    notification1.channel = channel
+
+    # Should be ignored as different channel
+    notification2 = Mock()
+    notification2.channel = f"{channel}-different"
+
+    app.register(channel)(handler)
+
+    # Raise exception to break infinite loop
+    app.connection.poll.side_effect = [None, StopIteration]
+    app.connection.notifies = [notification0, notification1]
+
+    with pytest.raises(StopIteration):
+        app.run()
+
+    assert handler.call_args_list == [
+        call(notification0.payload),
+        call(notification1.payload),
+    ]
