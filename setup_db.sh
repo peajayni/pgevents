3dadc1f0416d7dfee835b553b05557d740a245f8
peajayni <peajayni>
2020-06-12 23:44:32 +0100

    Setup postgres db

---
+++ setup_db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+psql -c "create database test_db;" -U postgres
+psql -c "create user test_user with encrypted password 'test_password';" -U postgres
+psql -c "grant all privileges on database test_db to test_user;" -U postgres
