766c95ed070031c42f564e2d95b1fab00453ea8d
peajayni <peajayni>
2020-06-13 12:07:22 +0100

    Replace integration test script

--- ci/integration_test.sh
+++ ci/integration_test.sh
@@ -1,46 +1,8 @@
 #!/bin/bash
 set -eufx
 
-BASE_DIR=$(dirname "$0")
+psql -U postgres -c "create database test"
+psql -U postgres -c "create user test with encrypted password 'test'"
+psql -U postgres -c "grant all privileges on database test to test"
 
-DB_CONTAINER_NAME="integration_db"
-
-TEST_IMAGE_NAME="test"
-TEST_CONTAINER_NAME="test_runner"
-
-NETWORK_NAME="test"
-
-function clean_up {
-  set +e
-  docker stop ${DB_CONTAINER_NAME}
-  docker network rm ${NETWORK_NAME}
-  set -e
-}
-
-trap clean_up EXIT
-
-clean_up
-
-docker network create ${NETWORK_NAME}
-
-docker run \
-    --rm \
-    -d \
-    --name ${DB_CONTAINER_NAME} \
-    --network ${NETWORK_NAME} \
-    -e POSTGRES_PASSWORD=postgres \
-    -e POSTGRES_USER=postgres \
-    -e POSTGRES_PASSWORD=postgres \
-    postgres
-
-echo "Waiting for postgres to start"
-sleep 10s
-
-docker build --tag ${TEST_IMAGE_NAME} "${BASE_DIR}/.."
-
-docker run \
-  --rm \
-  --network ${NETWORK_NAME} \
-  --name ${TEST_CONTAINER_NAME} \
-  ${TEST_IMAGE_NAME} \
-  pytest tests/integration
+pytest --cov=pgevents tests/integration
--- ci/integration_test2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eufx
-
-psql -U postgres -c "create database test"
-psql -U postgres -c "create user test with encrypted password 'test'"
-psql -U postgres -c "grant all privileges on database test to test"
-
-pytest --cov=pgevents tests/integration
