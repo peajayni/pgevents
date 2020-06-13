fd0ec4509b5c86283802b71fbbb5173ea7d3449e
peajayni <peajayni>
2020-06-13 02:29:23 +0100

    Add real integration test

--- ci/integration_test.sh
+++ ci/integration_test.sh
@@ -2,16 +2,22 @@
 set -eufx
 
 BASE_DIR=$(dirname "$0")
-DB_IMAGE_NAME=integration_db
-TEST_IMAGE_NAME=test
-NETWORK_NAME=test
+
+DB_CONTAINER_NAME="integration_db"
+
+TEST_IMAGE_NAME="test"
+TEST_CONTAINER_NAME="test_runner"
+
+NETWORK_NAME="test"
 
 function clean_up {
   set +e
-  docker stop ${DB_IMAGE_NAME}
+  docker stop ${DB_CONTAINER_NAME}
   docker network rm ${NETWORK_NAME}
   set -e
 }
+
+trap clean_up EXIT
 
 clean_up
 
@@ -20,7 +26,7 @@ docker network create ${NETWORK_NAME}
 docker run \
     --rm \
     -d \
-    --name ${DB_IMAGE_NAME} \
+    --name ${DB_CONTAINER_NAME} \
     --network ${NETWORK_NAME} \
     -e POSTGRES_PASSWORD=postgres \
     -e POSTGRES_USER=postgres \
@@ -32,11 +38,9 @@ sleep 10s
 
 docker build --tag ${TEST_IMAGE_NAME} "${BASE_DIR}/.."
 
-set +e
-docker run --rm --network ${NETWORK_NAME} ${TEST_IMAGE_NAME} pytest tests/integration
-TEST_EXIT=$?
-set -e
-
-clean_up
-
-exit ${TEST_EXIT}
+docker run \
+  --rm \
+  --network ${NETWORK_NAME} \
+  --name ${TEST_CONTAINER_NAME} \
+  ${TEST_IMAGE_NAME} \
+  pytest tests/integration
--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -1,8 +1,35 @@
+from threading import Thread
+
+import psycopg2
+
 from pgevents.app import App
+
+DSN = "dbname=postgres user=postgres password=postgres host=integration_db"
+CHANNEL = "foo"
+
+
+class EndTest(Exception):
+    pass
+
+
+def send_notification():
+    connection = psycopg2.connect(DSN)
+    connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = connection.cursor()
+    cursor.execute(f"NOTIFY {CHANNEL}")
 
 
 def test_app():
-    dsn = "dbname=postgres user=postgres password=postgres host=integration_db"
-    app = App(dsn)
-    cursor = app.connection.cursor()
-    cursor.execute("SELECT 1")
+    app = App(DSN)
+
+    @app.register(CHANNEL)
+    def handler(payload):
+        raise EndTest
+
+    thread = Thread(target=send_notification)
+    thread.start()
+
+    try:
+        app.run()
+    except EndTest:
+        pass
