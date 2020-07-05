f8cb035382f536f5b7fa11e0dc1e1a23d81e3211
peajayni <peajayni>
2020-07-05 01:03:50 +0100

    Fix whitespace

--- pgevents/migrations/20200704_2145_migration_create_event_table/apply.sql
+++ pgevents/migrations/20200704_2145_migration_create_event_table/apply.sql
@@ -1,10 +1,10 @@
 CREATE TYPE event_status AS ENUM ('PENDING', 'PROCESSED');
 
 CREATE TABLE events (
-	id bigserial PRIMARY KEY,
-	topic varchar(50) NOT NULL,
-	status event_status NOT NULL DEFAULT 'PENDING',
-	payload json,
-	created_at timestamp NOT NULL DEFAULT now(),
+    id bigserial PRIMARY KEY,
+    topic varchar(50) NOT NULL,
+    status event_status NOT NULL DEFAULT 'PENDING',
+    payload json,
+    created_at timestamp NOT NULL DEFAULT now(),
     processed_at timestamp
 );
