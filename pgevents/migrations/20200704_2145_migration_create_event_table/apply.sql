97ea237a933690c19f8729c234eb8605fa596731
peajayni <peajayni>
2020-07-12 01:58:34 +0100

    Convert payload field to jsonb

--- pgevents/migrations/20200704_2145_migration_create_event_table/apply.sql
+++ pgevents/migrations/20200704_2145_migration_create_event_table/apply.sql
@@ -4,7 +4,7 @@ CREATE TABLE events (
     id bigserial PRIMARY KEY,
     topic varchar(50) NOT NULL,
     status event_status NOT NULL DEFAULT 'PENDING',
-    payload json,
+    payload jsonb,
     created_at timestamp with time zone NOT NULL DEFAULT now(),
     process_after timestamp with time zone NOT NULL DEFAULT now(),
     processed_at timestamp with time zone
